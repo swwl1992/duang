e7de6fb8efd555893c77234ca8a95d6f1ba030b5
swwl1992 <swwl1992@gmail.com>
2015-03-13 16:59:19 +0800

    init commit

---
+++ js/duang.js
@@ -0,0 +1,26 @@
+function bindButtonClick() {
+	$('input#play-button').click(play);
+}
+
+function play() {
+	var zone = document.getElementById('audio-element');
+	var audio = initAudioElt();
+	zone.appendChild(audio);
+	audio.play();
+	// remove audio element when complete
+	setTimeout(function() {
+		zone.removeChild(audio);
+	}, 4000);
+}
+
+function initAudioElt() {
+	var elt = document.createElement('audio');
+	elt.setAttribute('preload', 'auto');
+	elt.setAttribute('autoplay', 'autoplay');
+	elt.setAttribute('src', 'audio/duang.mp3');
+	return elt;
+}
+
+$(document).ready(function() {
+	bindButtonClick();
+});
